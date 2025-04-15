037d4ab55862b98de9cf753f7d34223b58895820
matiasdata <matiasdata@gmail.com>
2025-04-15 08:37:20 +0100

    PathDependant interface

---
+++ PathDependent/include/PathDependent.h
@@ -0,0 +1,24 @@
+#include <MyArray.h>
+#include <vector>
+
+class CashFlow
+{
+public:    
+    CashFlow(unsigned long TimeIndex_ = 0UL, double Amount_ = 0.0) : TimeIndex{TimeIndex_}, Amount{Amount_} {};
+    unsigned long TimeIndex;
+    double Amount;
+};
+
+class PathDependent
+{
+public:
+    PathDependent(MyArray& LookAtTimes);
+    const MyArray& GetLookAtTimes() const;
+    virtual unsigned long MaxNumberOfCashFlows() const = 0;
+    virtual MyArray PossibleCashFlowTimes() const = 0;
+    virtual unsigned long CashFlows(const MyArray& SpotValues, std::vector<CashFlow>& GeneratedCashFlows) const = 0;
+    virtual PathDependent* clone() const = 0;
+    virtual ~PathDependent(){};
+private:
+    MyArray LookAtTimes;
+};
