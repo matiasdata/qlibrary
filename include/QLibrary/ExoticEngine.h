c327e2ca78b783ef8937a203c2d8a0217796195d
matiasdata <matiasdata@gmail.com>
2025-04-19 14:19:27 +0100

    Add ExoticEngine base class

---
+++ include/QLibrary/ExoticEngine.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <QLibrary/Wrapper.h>
+#include <QLibrary/Parameters.h>
+#include <QLibrary/PathDependent.h>
+#include <QLibrary/MCStatistics.h>
+#include <vector>
+
+class ExoticEngine
+{
+public:
+    ExoticEngine(const Wrapper<PathDependent>& TheProduct_, const Wrapper<Parameters>& r_);
+    virtual void GetOnePath(MyArray& SpotValues) = 0;
+    void DoSimulation(MCStatistics<double>& TheGatherer, unsigned long NumberOfPaths);
+    virtual ~ExoticEngine(){};
+    double DoOnePath(const MyArray& SpotValues) const;
+private:
+    Wrapper<PathDependent> TheProduct;
+    Wrapper<Parameters> r;
+    MyArray Discounts;
+    mutable std::vector<CashFlow> TheseCashFlows;
+};
