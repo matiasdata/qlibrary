23cca20dfe251d9c5636d20c6b6b3e3a7a0e0d11
matiasdata <matiasdata@gmail.com>
2025-04-19 15:09:37 +0100

    Add ExoticBSEngine

---
+++ include/QLibrary/ExoticBSEngine.h
@@ -0,0 +1,25 @@
+#include <QLibrary/ExoticEngine.h>
+#include <QLibrary/RandomGen.h>
+
+
+class ExoticBSEngine : public ExoticEngine
+{
+public:
+    ExoticBSEngine(const Wrapper<PathDependent>& TheProduct_,
+                    const Wrapper<Parameters>& R_,
+                    const Wrapper<Parameters>& D_,
+                    const Wrapper<Parameters>& Vol_,
+                    const Wrapper<RandomBase>& TheGenerator_,
+                    double Spot_);
+    
+    virtual void GetOnePath(MyArray& SpotValues);
+    virtual ~ExoticBSEngine(){};
+private:
+        Wrapper<RandomBase> TheGenerator;
+        MyArray Drifts;
+        MyArray StandardDeviations;
+        double LogSpot;
+        unsigned long NumberOfTimes;
+        MyArray Variates;
+
+};
---
+++ src/QLibrary/ExoticBSEngine.cpp
@@ -0,0 +1,43 @@
+#include <QLibrary/ExoticBSEngine.h>
+#include <cmath>
+
+ExoticBSEngine::ExoticBSEngine(const Wrapper<PathDependent>& TheProduct_,
+        const Wrapper<Parameters>& R_,
+        const Wrapper<Parameters>& D_,
+        const Wrapper<Parameters>& Vol_,
+        const Wrapper<RandomBase>& TheGenerator_,
+        double Spot_) : ExoticEngine{TheProduct_,R_}, TheGenerator{TheGenerator_}
+{
+    MyArray Times(TheProduct_->GetLookAtTimes());
+    NumberOfTimes = Times.size();
+
+    TheGenerator->resetDimensionality(NumberOfTimes);
+    Drifts.resize(NumberOfTimes);
+    StandardDeviations.resize(NumberOfTimes);
+
+    double Variance = Vol_->IntegralSquare(0.0,Times[0]);
+    Drifts[0] = R_->Integral(0.0,Times[0]) - D_->Integral(0.0,Times[0]) - 0.5 * Variance;
+    StandardDeviations[0] = sqrt(Variance);
+
+    for(unsigned long j = 1; j < NumberOfTimes; ++j)
+    {
+        double ThisVariance = Vol_->IntegralSquare(Times[j-1],Times[j]);
+        Drifts[j] = R_->Integral(Times[j-1],Times[j]) - D_->Integral(Times[j-1],Times[j]) - 0.5 * ThisVariance;
+        StandardDeviations[j] = sqrt(ThisVariance);
+    }
+    LogSpot = log(Spot_);
+    Variates.resize(NumberOfTimes);
+}
+
+void ExoticBSEngine::GetOnePath(MyArray& SpotValues)
+{
+    TheGenerator->getGaussians(Variates);
+    double CurrentLogSpot = LogSpot;
+    for(unsigned long j = 0; j < NumberOfTimes; j++)
+    {
+        CurrentLogSpot += Drifts[j];
+        CurrentLogSpot += StandardDeviations[j] * Variates[j];
+        SpotValues[j] = exp(CurrentLogSpot);
+    }
+    return;
+}
