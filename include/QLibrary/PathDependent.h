94d8a4dc16888b79e3d30e7552612c47fdffdf41
matiasdata <matiasdata@gmail.com>
2025-04-21 10:57:13 +0100

    Update QLibrary

--- include/QLibrary/PathDependent.h
+++ include/QLibrary/PathDependent.h
@@ -23,3 +23,22 @@ public:
 private:
     MyArray LookAtTimes;
 };
+
+/*
+PathDependent is a virtual base class for PathDependent Exotic (but European) options.
+
+LookAtTimes is an array that stores all times where the option can Look at the Spot value.
+
+const MyArray& GetLookAtTimes() const: returns the LookAtTimes array but being const it can't be modified.
+
+virtual unsigned long MaxNumberOfCashFlows() const = 0: (virtual) returns the maximum number of CashFlows that the option can have.
+
+virtual MyArray PossibleCashFlowTimes() const = 0: (virtual) returns the possible CashFlow times (i.e. when cashflows can happen) in an array.
+
+virtual unsigned long CashFlows(const MyArray& SpotValues, std::vector<CashFlow>& GeneratedCashFlows) const = 0:
+    Computes the CashFlows of the options using the SpotValues given and stores these in the GeneratedCashFlows.
+
+
+
+
+*/
