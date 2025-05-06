68f504c871de0dca6800338d1309c7700acacccf
matiasdata <matiasdata@gmail.com>
2025-05-06 08:42:28 +0100

    Add Binomial Tree

--- src/QLibrary/BinomialTree.cpp
+++ src/QLibrary/BinomialTree.cpp
@@ -20,4 +20,31 @@ SimpleBinomialTree::SimpleBinomialTree(double Spot_,
     TreeBuilt = false;
 }
 
+void SimpleBinomialTree::BuildTree()
+{
+    TreeBuilt = true;
+    TheTree.resize(Steps+1);
+    double InitialLogSpot = log(Spot);
+
+    for(unsigned long i = 0; i <= Steps; i++)
+    {
+        TheTree[i].resize(i+1);
+        double thisTime = (i*Time)/Steps;
+
+        double movedLogSpot = InitialLogSpot + r.Integral(0.0, thisTime) - q.Integral(0.0,thisTime);
+        movedLogSpot -= 0.5 * Volatility * Volatility * thisTime;
+        double std = Volatility * std::sqrt(Time/Steps);
+        
+        for(long j = -static_cast<long>(i), k = 0; j <= static_cast<long>(i); j = j+2, k++)
+        {
+            TheTree[i][k].first = std::exp(movedLogSpot + j * std);
+        }
+    }
+
+    for(unsigned long l = 0; l < Steps; l++)
+    {
+        Discounts[l] = std::exp(-r.Integral((l*Time)/Steps, ((l+1)*Time)/Steps))
+    }
+}
+
 }
