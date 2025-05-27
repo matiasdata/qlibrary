d29e996b976f1c96d20a89a53a332937f127a784
matiasdata <matiasdata@gmail.com>
2025-05-27 08:29:22 +0100

    Add NumericalIntegration.h

---
+++ include/QLibrary/NumericalIntegration.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cmath>
+
+namespace QLibrary
+{
+    
+template <typename T>
+double TrapezoidalRule(double a, double b, unsigned long N, T TheFunction)
+{
+    double integral = 0;
+    double deltax = (b-a)/static_cast<double>(N);
+    double x = a;
+    integral += TheFunction(x)/2;
+    for(unsigned long i = 1; i < N; i++)
+    {
+        x += deltax;
+        integral += TheFunction(x);
+    }
+    x = b;
+    integral += TheFunction(x)/2;
+    integral *= deltax;
+    return integral;
+}
+
+
+} // namespace QLibrary
