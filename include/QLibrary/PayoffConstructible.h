42e52c7d859332e4c6e2710e862d53869f773de1
matiasdata <matiasdata@gmail.com>
2025-05-30 08:09:48 +0100

    Add PayoffConstructible.h

---
+++ include/QLibrary/PayoffConstructible.h
@@ -0,0 +1,33 @@
+// PayoffConstructible.h
+#pragma once
+
+
+#include <QLibrary/Payoff.h>
+#include <QLibrary/PayoffFactory.h>
+#include <iostream>
+#include <string>
+
+namespace QLibrary{
+
+template <typename T>
+class PayoffHelper
+{
+public:
+    PayoffHelper(std::string);
+    static Payoff* Create(double);
+};
+
+template <typename T>
+PayoffHelper<T>::PayoffHelper(std::string PayoffId)
+{
+    PayoffFactory& thePayoffFactory = PayoffFactory.Instance();
+    thePayoffFactory.RegisterPayoff(PayoffId, PayoffHelper<T>::Create);
+}
+
+template <typename T>
+Payoff* PayoffHelper<T>::Create(double Strike)
+{
+    return new T(Strike);
+}
+
+} // namespace QLibrary
