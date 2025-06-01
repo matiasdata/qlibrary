27b6611d2431801f6cac3d27953aa576cecb54b0
matiasdata <matiasdata@gmail.com>
2025-06-01 09:18:35 +0100

    Integrate the factory with a Monte Carlo routine

--- Tests/PayFactory/main.cpp
+++ Tests/PayFactory/main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <QLibrary/SimpleMC.h>
 
 int main()
 {
@@ -29,9 +30,27 @@ int main()
     if(PayoffPtr != nullptr)
     {
         double Spot;
-        std::cout << "\nspot\n";
+        double Vol_;
+        double r_;
+        double Expiry;
+        unsigned long NumberOfPaths = 1000000;
+        std::cout << "Enter the Spot:" << std::endl;
         std::cin >> Spot;
-        std::cout << "\n" << PayoffPtr->operator()(Spot) << "\n";
+        std::cout << "Enter the Expiry:" << std::endl;
+        std::cin >> Expiry;
+        std::cout << "Enter the volatility:" << std::endl;
+        std::cin >> Vol_;
+        std::cout << "Enter the risk free rate:" << std::endl;
+        std::cin >> r_;
+        QLibrary::parametersConstant Vol{Vol_};
+        QLibrary::parametersConstant r{r_};
+
+        
+        QLibrary::RandomMLCG rg(1,0);
+        QLibrary::MCStatisticsMean gatherer_call;
+        QLibrary::VanillaOption callOption(*PayoffPtr,Expiry);
+        QLibrary::SimpleMonteCarlo(callOption, Spot, Vol, r, NumberOfPaths,gatherer_call,rg);
+        std::cout << "Monte Carlo price of a call: " << gatherer_call.getResults()["mean"] << std::endl;
         delete PayoffPtr;
     }
     return 0;
